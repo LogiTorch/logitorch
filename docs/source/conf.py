a9a8a9b745c06b5eee582c67de57d2d262b6b750
ChadiHelwe <chadi.helwe@gmail.com>
2021-11-21 03:40:08 +0100

    updated conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2021, Chadi Helwe'
 author = 'Chadi Helwe'
 
 # The full version, including alpha/beta/rc tags
-release = '0.1.0'
+release = '0.0.1'
 
 
 # -- General configuration ---------------------------------------------------
