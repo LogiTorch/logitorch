ca2ad4f92179e59b4c71a4a10a32e83598366fd6
ChadiHelwe <chadi.helwe@gmail.com>
2021-12-11 01:58:56 +0100

    Added Exceptions

--- torchtextlogic/datasets/utils.py
+++ torchtextlogic/datasets/utils.py
@@ -1,4 +1,32 @@
-def download_dataset(url: str) -> None:
+import os
+from zipfile import ZipFile
+
+import requests
+
+from tqdm import tqdm
+
+DATASET_FOLDER_NAME = "dataset/"
+class EmbeddingFileNameError(Exception):
+    """
+    Raised when a pretrained word embedding file name is wrong
+    """
+
+    def __init__(self):
+        self.message = (
+            "EmbeddingFileNameError: Pretrained word embedding file name is wrong"
+        )
+
+
+class FileSizeError(Exception):
+    """
+    Raised when a downloaded file has a wrong size
+    """
+
+    def __init__(self):
+        self.message = "FileSizeError: Wrong file size"
+
+
+def download_dataset(url: str, src_dataset: str) -> None:
     pass
 
 
