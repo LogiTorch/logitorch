cb88bff087b42e43fd0d2e6e22a7adfd49b135e7
Chadi <chadi.helwe@gmail.com>
2021-12-13 19:46:03 +0100

    Added AbstractQADataset and AbstractTEDataset

--- torchtextlogic/datasets/abstract_dataset.py
+++ torchtextlogic/datasets/abstract_dataset.py
@@ -4,7 +4,7 @@ from typing import Any, Tuple
 from torch.utils.data import Dataset
 
 
-class AbstractDataset(Dataset, ABC):
+class AbstractQADataset(Dataset, ABC):
     def __init__(self) -> None:
         super().__init__()
 
@@ -13,7 +13,28 @@ class AbstractDataset(Dataset, ABC):
         pass
 
     @abstractmethod
-    def __getitem__(self, index: int) -> Tuple[str, Any]:
+    def __getitem__(self, index: int) -> Tuple[str, str, str, Any]:
+        pass
+
+    @abstractmethod
+    def __len__(self) -> int:
+        pass
+
+    @abstractmethod
+    def __str__(self) -> str:
+        pass
+
+
+class AbstracTEDataset(Dataset, ABC):
+    def __init__(self) -> None:
+        super().__init__()
+
+    @abstractmethod
+    def read_dataset(self, dataset_name: str) -> None:
+        pass
+
+    @abstractmethod
+    def __getitem__(self, index: int) -> Tuple[str, str, Any]:
         pass
 
     @abstractmethod
--- torchtextlogic/datasets/control_dataset.py
+++ torchtextlogic/datasets/control_dataset.py
@@ -1,16 +1,16 @@
 from typing import Any, Tuple
 
-from abstract_dataset import AbstractDataset
+from abstract_dataset import AbstractTEDataset
 
 
-class ControlDataset(AbstractDataset):
+class ControlDataset(AbstractTEDataset):
     def __init__(self) -> None:
         super().__init__()
 
     def read_dataset(self, dataset_name: str) -> None:
         pass
 
-    def __getitem__(self, index: int) -> Tuple[str, Any]:
+    def __getitem__(self, index: int) -> Tuple[str, str, Any]:
         pass
 
     def __str__(self) -> str:
