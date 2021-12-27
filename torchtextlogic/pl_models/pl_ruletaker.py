55b3bee05c79ba9de18c0e3340d6a3775ed26026
Chadi <chadi.helwe@gmail.com>
2021-12-27 17:19:15 +0200

    added learning_rate in PLRuleTaker

--- torchtextlogic/pl_models/pl_ruletaker.py
+++ torchtextlogic/pl_models/pl_ruletaker.py
@@ -5,16 +5,17 @@ from torchtextlogic.models.ruletaker import RuleTaker
 
 
 class PLRuleTaker(pl.LightningModule):
-    def __init__(self, pretrained_model: str) -> None:
+    def __init__(self, pretrained_model: str, learning_rate: float = 1e-3) -> None:
         super().__init__()
         self.model = RuleTaker(pretrained_model)
+        self.learning_rate = learning_rate
         self.cross_entropy_loss = nn.CrossEntropyLoss()
 
     def forward(self, **x):
         return self.model(**x)
 
     def configure_optimizers(self):
-        return Adam(self.parameters(), lr=1e-3)
+        return Adam(self.parameters(), lr=self.learning_rate)
 
     def training_step(self, train_batch, batch_idx):
         x, y = train_batch
