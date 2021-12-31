26740285f2b89747bec76b1d609746e39e8bdda9
Chadi <chadi.helwe@gmail.com>
2021-12-31 18:33:40 +0200

    added docstrings in pl_ruletaker.py but not filled

--- torchtextlogic/pl_models/pl_ruletaker.py
+++ torchtextlogic/pl_models/pl_ruletaker.py
@@ -9,19 +9,42 @@ from transformers.modeling_outputs import SequenceClassifierOutput
 
 
 class PLRuleTaker(pl.LightningModule):
+    """[summary]
+    """    
     def __init__(self, pretrained_model: str, learning_rate: float = 1e-3) -> None:
+        """[summary]
+
+        :param pretrained_model: [description]
+        :type pretrained_model: str
+        :param learning_rate: [description], defaults to 1e-3
+        :type learning_rate: float, optional
+        """        
         super().__init__()
         self.model = RuleTaker(pretrained_model)
         self.learning_rate = learning_rate
         self.cross_entropy_loss = nn.CrossEntropyLoss()
 
     def forward(self, **x: Dict[str, torch.Tensor]) -> SequenceClassifierOutput:  # type: ignore
+        """[summary]
+
+        :return: [description]
+        :rtype: SequenceClassifierOutput
+        """        
         return self.model(**x)
 
     def configure_optimizers(self):
         return Adam(self.parameters(), lr=self.learning_rate)
 
     def training_step(self, train_batch: Tuple[Dict[str, torch.Tensor], torch.Tensor], batch_idx: int) -> torch.Tensor:  # type: ignore
+        """[summary]
+
+        :param train_batch: [description]
+        :type train_batch: Tuple[Dict[str, torch.Tensor], torch.Tensor]
+        :param batch_idx: [description]
+        :type batch_idx: int
+        :return: [description]
+        :rtype: torch.Tensor
+        """        
         x, y = train_batch
         outputs = self(**x)
         y_pred = outputs.logits
@@ -30,6 +53,13 @@ class PLRuleTaker(pl.LightningModule):
         return loss
 
     def validation_step(self, val_batch: Tuple[Dict[str, torch.Tensor], torch.Tensor], batch_idx: int) -> None:  # type: ignore
+        """[summary]
+
+        :param val_batch: [description]
+        :type val_batch: Tuple[Dict[str, torch.Tensor], torch.Tensor]
+        :param batch_idx: [description]
+        :type batch_idx: int
+        """        
         x, y = val_batch
         outputs = self(**x)
         y_pred = outputs.logits
@@ -37,6 +67,13 @@ class PLRuleTaker(pl.LightningModule):
         self.log("val_loss", loss, on_epoch=True)
 
     def test_step(self, val_batch: Tuple[Dict[str, torch.Tensor], torch.Tensor], batch_idx: int) -> None:  # type: ignore
+        """[summary]
+
+        :param val_batch: [description]
+        :type val_batch: Tuple[Dict[str, torch.Tensor], torch.Tensor]
+        :param batch_idx: [description]
+        :type batch_idx: int
+        """        
         x, y = val_batch
         outputs = self(**x)
         y_pred = outputs.logits
@@ -44,5 +81,16 @@ class PLRuleTaker(pl.LightningModule):
         self.log("val_loss", loss, on_epoch=True)
 
     def predict_step(self, batch: Dict[str, torch.Tensor], batch_idx: int, dataloader_idx: Optional[int] = 0) -> torch.Tensor:  # type: ignore
+        """[summary]
+
+        :param batch: [description]
+        :type batch: Dict[str, torch.Tensor]
+        :param batch_idx: [description]
+        :type batch_idx: int
+        :param dataloader_idx: [description], defaults to 0
+        :type dataloader_idx: Optional[int], optional
+        :return: [description]
+        :rtype: torch.Tensor
+        """        
         outputs = self(**batch)
         return outputs.logits
