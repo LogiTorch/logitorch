7de8da87b2154db818db37eec3e52976fb169cb8
Chadi <chadi.helwe@gmail.com>
2022-01-02 23:04:53 +0200

    added constant variables in proofwriter_dataset.py

--- torchtextlogic/datasets/proofwriter_dataset.py
+++ torchtextlogic/datasets/proofwriter_dataset.py
@@ -8,3 +8,26 @@ from torchtextlogic.datasets.utils import (
     download_dataset,
     read_jsonl,
 )
+
+PROOFWRITER_DATASET_ZIP_URL = (
+    "https://www.dropbox.com/s/5dpm2yjcm5h9hsd/proofwriter_dataset.zip?dl=1"
+)
+PROOFWRITER_DATASET = "proofwriter_dataset"
+PROOFWRITER_SUB_DATASETS = [
+    "birds-electricity",
+    "depth-0",
+    "depth-1",
+    "depth-2",
+    "depth-3",
+    "depth-3ext",
+    "depth-3ext-NatLang",
+    "depth-5",
+    "NatLang",
+]
+PROOFWRITER_DATASET_FOLDER = f"{DATASETS_FOLDER}/{PROOFWRITER_DATASET}"
+PROOFWRITER_LABEL_TO_ID = {False: 0, True: 1}
+PROOFWRITER_ID_TO_LABEL = {0: "False", 1: "True"}
+
+
+class ProofWriterDataset:
+    pass
