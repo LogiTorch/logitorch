1f917c188431f25756324838783648bf2bcf37f3
ChadiHelwe <chadi.helwe@gmail.com>
2022-03-05 23:29:14 +0100

    add TaskError

--- torchtextlogic/datasets/exceptions.py
+++ torchtextlogic/datasets/exceptions.py
@@ -28,3 +28,12 @@ class FileSizeError(Exception):
 
     def __init__(self):
         self.message = "FileSizeError: Wrong file size"
+
+
+class TaskError(Exception):
+    """
+    An error is raised when the task is wrong
+    """
+
+    def __init__(self, tasks: List[str]):
+        self.message = f"TaskError: Task is wrong\nThe tasks are: {tasks}"
