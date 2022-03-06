eb7ae6a7cf4560c9d255490158e31e493d0b2724
ChadiHelwe <chadi.helwe@gmail.com>
2022-03-06 01:26:50 +0100

    update exceptions.py

--- torchtextlogic/datasets/exceptions.py
+++ torchtextlogic/datasets/exceptions.py
@@ -35,8 +35,8 @@ class TaskError(Exception):
     An error is raised when the task is wrong
     """
 
-    def __init__(self, tasks: List[str]):
-        self.message = f"TaskError: Task is wrong\nThe tasks are: {tasks}"
+    def __init__(self):
+        self.message = f"TaskError: Task is not found\n"
 
 
 class AbductionClosedWorldAssumptionError(Exception):
