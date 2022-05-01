37fb13c30c3eef72bdaefab7d797111b733d93e4
Chadi <chadi.helwe@gmail.com>
2022-05-01 05:32:49 +0200

    implment CoreferenceResolution but not finish

--- torchtextlogic/utilities/coreference_resolution.py
+++ torchtextlogic/utilities/coreference_resolution.py
@@ -1,3 +1,27 @@
+import logging
+import sys
+from typing import Dict, Tuple
+
+from allennlp_models import pretrained
+
+logging.disable(sys.maxsize)
+
+
 class CoreferenceResolution:
     def __init__(self) -> None:
+        self.model = pretrained.load_predictor("coref-spanbert")
+
+    def predict(self, passage: str) -> Dict(Tuple(int, int), str, str):
         pass
+
+    def coref_resolved(self, passage: str) -> str:
+        return self.model.coref_resolved(passage)
+
+
+if __name__ == "__main__":
+    analyzer = CoreferenceResolution()
+    print(
+        analyzer.coref_resolved(
+            "Zidane played football. He was one of the greatest footballer."
+        )
+    )
