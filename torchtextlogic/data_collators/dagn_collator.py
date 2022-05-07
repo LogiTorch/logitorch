b45e70c94be366c0052ca1e6963938a986b5effd
Chadi <chadi.helwe@gmail.com>
2022-05-07 05:21:08 +0200

    orking on dagn

--- torchtextlogic/data_collators/dagn_collator.py
+++ torchtextlogic/data_collators/dagn_collator.py
@@ -1,12 +1,12 @@
-from typing import Dict, Tuple
+from typing import Dict, Tuple, List
 
 import torch
-from transformers import AutoTokenizer
+from transformers import AutoTokenizer, RobertaTokenizer
 
 
 class DAGNCollator:
     def __init__(self, pretrained_tokenizer: str) -> None:
-        self.tokenizer = AutoTokenizer.from_pretrained(pretrained_tokenizer)
+        self.tokenizer = RobertaTokenizer.from_pretrained(pretrained_tokenizer)
 
     def __call__(self, batch) -> Tuple[Dict[str, torch.Tensor], torch.Tensor]:
         contexts = []
@@ -21,3 +21,20 @@ class DAGNCollator:
 
         batch_x = self.tokenizer(contexts, questions, padding=True, return_tensors="pt")
         return batch_x, torch.tensor(batch_y, dtype=torch.int64)
+
+def find_explicit_connectives(context: str, question: str, answers_list: List[str]):
+    tokenizer = RobertaTokenizer.from_pretrained("roberta-large")
+    tokenized_context = tokenizer.tokenize(context)
+    tokenized_question = tokenizer.tokenize(question)
+    tokenized_answers_list  = []
+    for ans in answers_list:
+        tokenized_answers_list.append(tokenizer.tokenize(ans))
+    
+    print(tokenized_context)
+    print(tokenized_question)
+    print(tokenized_answers_list)
+    
+
+find_explicit_connectives("<s> Hello world</s>", "I play football", ["youpi </s>"])
+# def __find_puncts():
+#     pass
---
+++ torchtextlogic/models/dagn.py
@@ -0,0 +1,9 @@
+import torch.nn as nn
+from transformers import AutoConfig, RobertaConfig
+
+class DAGN(nn.Module):
+    def __init__(self, pretrained_roberta_model) -> None:
+        super().__init__()
+        self.roberta_model = RobertaConfig.from_pretrained(pretrained_roberta_model)
+        
+
