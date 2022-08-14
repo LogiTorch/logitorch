b5fe19685ee7e70a01ad8648e4aa671f09259e01
Chadi <chadi.helwe@gmail.com>
2022-08-15 01:44:01 +0300

    update documentation

--- logitorch/data_collators/ruletaker_collator.py
+++ logitorch/data_collators/ruletaker_collator.py
@@ -8,11 +8,22 @@ from logitorch.datasets.proof_qa.proofwriter_dataset import PROOFWRITER_LABEL_TO
 
 class RuleTakerCollator:
     def __init__(self) -> None:
+        """
+        The function __init__() is a constructor that initializes the tokenizer variable to the
+        RobertaTokenizer.from_pretrained() function
+        """
         self.tokenizer = RobertaTokenizer.from_pretrained(
             "LIAMF-USP/roberta-large-finetuned-race"
         )
 
     def __call__(self, batch) -> Tuple[Dict[str, torch.Tensor], torch.Tensor]:
+        """
+        The function takes in a batch of data, and returns a tuple of two elements: the first element is
+        a dictionary of tensors, and the second element is a tensor
+
+        :param batch: A list of tuples of the form (context, question, label, depth)
+        :return: A tuple of two tensors.
+        """
         contexts = []
         questions = []
         batch_y = []
@@ -28,11 +39,22 @@ class RuleTakerCollator:
 
 class RuleTakerProofWriterCollator:
     def __init__(self) -> None:
+        """
+        The function __init__() is a constructor that initializes the tokenizer variable to the
+        RobertaTokenizer.from_pretrained() function
+        """
         self.tokenizer = RobertaTokenizer.from_pretrained(
             "LIAMF-USP/roberta-large-finetuned-race"
         )
 
     def __call__(self, batch) -> Tuple[Dict[str, torch.Tensor], torch.Tensor]:
+        """
+        It takes a batch of data and returns a tuple of two tensors. The first tensor is a dictionary of
+        tensors, and the second tensor is a tensor of labels.
+
+        :param batch: A list of tuples of the form (context, question, label)
+        :return: A tuple of two tensors.
+        """
         contexts = []
         questions = []
         labels = []
