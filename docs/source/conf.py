62e09f550cde2ef2f09770fc817fb35bac4aca34
Chadi <chadi.helwe@gmail.com>
2022-08-15 20:52:49 +0300

    update release version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = "2021, Chadi Helwe"
 author = "Chadi Helwe"
 
 # The full version, including alpha/beta/rc tags
-release = "0.0.0"
+release = "0.0.1a1"
 
 
 # -- General configuration ---------------------------------------------------
