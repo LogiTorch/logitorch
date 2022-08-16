e559f5a48a4a1ab4cb4d36e515a2b0b65e2e8ab3
Chadi <chadi.helwe@gmail.com>
2022-08-17 00:38:53 +0300

    update tests

--- tests/datasets/test_utils.py
+++ tests/datasets/test_utils.py
@@ -4,7 +4,7 @@ from logitorch.datasets.utils import DATASETS_FOLDER, download_dataset
 
 
 def test_download_dataset() -> None:
-    url = "https://www.dropbox.com/s/znrr7a056zpxcxh/proofwriter_dataset.zip?dl=1"
+    url = "https://www.dropbox.com/s/5dpm2yjcm5h9hsd/proofwriter_dataset.zip?dl=1"
     dataset_name = "proofwriter_dataset"
     dataset_name_on_disk = f"{DATASETS_FOLDER}{dataset_name}"
     download_dataset(url, dataset_name)
