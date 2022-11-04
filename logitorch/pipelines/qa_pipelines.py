28f9d3d9acb78ebba7701210cfffc912329fd404
Chadi <chadi.helwe@gmail.com>
2022-11-04 15:23:11 +0100

    update qa_piples.py

--- logitorch/pipelines/qa_pipelines.py
+++ logitorch/pipelines/qa_pipelines.py
@@ -8,6 +8,8 @@ from logitorch.datasets.qa.ruletaker_dataset import RuleTakerDataset
 from logitorch.pipelines.exceptions import ModelNotCompatibleError
 from logitorch.pl_models.ruletaker import PLRuleTaker
 
+RULETAKER_COMPATIBLE_MODELS = PLRuleTaker
+
 
 def ruletaker_pipeline(
     model: nn.Module,
@@ -20,34 +22,37 @@ def ruletaker_pipeline(
     n_gpus: int,
 ):
     try:
-        if isinstance(model, PLRuleTaker):
-            train_dataset = RuleTakerDataset(dataset_name, "train")
-            val_dataset = RuleTakerDataset(dataset_name, "val")
+        if isinstance(model, RULETAKER_COMPATIBLE_MODELS):
+            if isinstance(model, PLRuleTaker):
+                train_dataset = RuleTakerDataset(dataset_name, "train")
+                val_dataset = RuleTakerDataset(dataset_name, "val")
 
-            ruletaker_collate_fn = RuleTakerCollator()
+                ruletaker_collate_fn = RuleTakerCollator()
 
-            train_dataloader = DataLoader(
-                train_dataset, batch_size=batch_size, collate_fn=ruletaker_collate_fn
-            )
-            val_dataloader = DataLoader(
-                val_dataset, batch_size=batch_size, collate_fn=ruletaker_collate_fn
-            )
+                train_dataloader = DataLoader(
+                    train_dataset,
+                    batch_size=batch_size,
+                    collate_fn=ruletaker_collate_fn,
+                )
+                val_dataloader = DataLoader(
+                    val_dataset, batch_size=batch_size, collate_fn=ruletaker_collate_fn
+                )
 
-            checkpoint_callback = ModelCheckpoint(
-                save_top_k=1,
-                monitor="val_loss",
-                mode="min",
-                dirpath=saved_model_path,
-                filename=saved_model_name,
-            )
+                checkpoint_callback = ModelCheckpoint(
+                    save_top_k=1,
+                    monitor="val_loss",
+                    mode="min",
+                    dirpath=saved_model_path,
+                    filename=saved_model_name,
+                )
 
-            trainer = pl.Trainer(
-                callbacks=[checkpoint_callback],
-                accelerator=accelerator,
-                gpus=n_gpus,
-                max_epochs=epochs,
-            )
-            trainer.fit(model, train_dataloader, val_dataloader)
+                trainer = pl.Trainer(
+                    callbacks=[checkpoint_callback],
+                    accelerator=accelerator,
+                    gpus=n_gpus,
+                    max_epochs=epochs,
+                )
+                trainer.fit(model, train_dataloader, val_dataloader)
         else:
             raise ModelNotCompatibleError()
 
