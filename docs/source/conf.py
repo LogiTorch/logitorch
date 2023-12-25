ad5b7645fec726c34a2f6050ee5ce868a3dc184b
Chadi Helwe <chadi.helwe@gmail.com>
2023-12-26 00:02:04 +0200

    update logo

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,7 +61,7 @@ html_theme = "furo"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
-html_logo = "logo.jpg"
+html_logo = "_static/logo.jpg"
 html_theme_options = {
     'logo_only': True,
     'display_version': False,
