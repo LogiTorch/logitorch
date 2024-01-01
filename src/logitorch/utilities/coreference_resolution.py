6307e83e1ab7b2d0a2c920b5fca7653188bb56e5
Chadi Helwe <chadihelwe@Chadis-MacBook-Air.local>
2024-01-02 01:45:27 +0200

    update package requirments

--- src/logitorch/utilities/coreference_resolution.py
+++ src/logitorch/utilities/coreference_resolution.py
@@ -1,33 +1,33 @@
-import logging
-import sys
-from typing import Dict, Tuple
+# import logging
+# import sys
+# from typing import Dict, Tuple
 
-from allennlp_models import pretrained
+# from allennlp_models import pretrained
 
-logging.disable(sys.maxsize)
-
-
-class CoreferenceResolution:
-    def __init__(self) -> None:
-        self.model = pretrained.load_predictor("coref-spanbert")
-
-    def coref_resolved(self, passage: str) -> str:
-        """
-        Resolves coreferences in the given passage.
-
-        Args:
-            passage (str): The input passage containing coreferences.
-
-        Returns:
-            str: The passage with resolved coreferences.
-        """
-        return self.model.coref_resolved(passage)
+# logging.disable(sys.maxsize)
 
 
-if __name__ == "__main__":
-    analyzer = CoreferenceResolution()
-    print(
-        analyzer.coref_resolved(
-            "Zidane played football. He was one of the greatest footballer."
-        )
-    )
+# class CoreferenceResolution:
+#     def __init__(self) -> None:
+#         self.model = pretrained.load_predictor("coref-spanbert")
+
+#     def coref_resolved(self, passage: str) -> str:
+#         """
+#         Resolves coreferences in the given passage.
+
+#         Args:
+#             passage (str): The input passage containing coreferences.
+
+#         Returns:
+#             str: The passage with resolved coreferences.
+#         """
+#         return self.model.coref_resolved(passage)
+
+
+# if __name__ == "__main__":
+#     analyzer = CoreferenceResolution()
+#     print(
+#         analyzer.coref_resolved(
+#             "Zidane played football. He was one of the greatest footballer."
+#         )
+#     )
